06438582b7333fa7e34900a43e3b60df1385bbb8
Ian Synge <gamersynge@gmail.com>
2025-09-13 13:01:56 -0400

    Added basic structure for the front end

---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "../styles/SearchBar.module.css";
+
+function SearchBar() {
+  return (
+    <div className={styles.searchbar}>
+    </div>
+  )
+}
+
+export default SearchBar
